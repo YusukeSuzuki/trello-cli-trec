66efb0381bfaae93790c30c616973c2281e5a9ca
Yusuke Suzuki <yusuke@geekfield.jp>
2024-05-08 20:05:35 +0900

    add board option to cards command

--- trec/api/boards.py
+++ trec/api/boards.py
@@ -3,10 +3,10 @@ from typing import Any, Dict, Optional
 import requests
 
 def create(
-	name: str, *args: Any, description: str=None, api_key: str, api_token: str, **kwargs: Any,
+  name: str, *args: Any, description: str=None, api_key: str, api_token: str, **kwargs: Any,
   ) -> Dict[str, any]:
 
-	# TODO: impl target workspace option
+  # TODO: impl target workspace option
   url = 'https://api.trello.com/1/boards'
   headers = { 'Accept': 'application/json' }
   query = { 'key': api_key, 'token': api_token }
@@ -14,5 +14,17 @@ def create(
   if description is not None:
     query = { 'desc': description, **query }
   response = requests.request('POST', url, headers=headers, params=query)
+
+  return response.json()
+
+
+def cards(
+  board_id: str, *, description: str=None, api_key: str, api_token: str, **kwargs: Any,
+  ) -> Dict[str, any]:
+
+  url = f'https://api.trello.com/1/board/{board_id}/cards'
+  headers = { 'Accept': 'application/json' }
+  query = { 'key': api_key, 'token': api_token,  }
+  response = requests.request('GET', url, headers=headers, params=query)
 
   return response.json()
--- trec/commands/cards/__init__.py
+++ trec/commands/cards/__init__.py
@@ -1,7 +1,12 @@
+import jmespath
 import requests
 import yaml
 
 from . import create
+
+import trec.api as api
+import trec.data as data
+import trec.utils.trello as trello_util
 
 
 sub_commands = [
@@ -16,25 +21,33 @@ def help():
   return 'card operations, if no subcommand, list all cards'
 
 def implement(parser):
-  parser.add_argument('--board', '-b', default=None)
-  parser.add_argument('--list', '-l')
+  parser.add_argument('--board', '-b', default=None,
+    help='specify board of cards to list up. name or trello id (wildcard available).')
+  # TODO: add list option
+  # TODO: add target option (The --target option cannot be used together with the --board or --list options.)
 
 def process(args):
-  headers = { 'Accept': 'application/json' }
-  query = { 'key': args.api_key, 'token': args.api_token,  }
+  db = data.db.load_or_setup(**vars(args))
 
-  url = 'https://api.trello.com/1/members/me/boards'
-  response = requests.request('GET', url, headers=headers, params=query)
-  boards = response.json()
+  board_id_query_parameter = (
+    trello_util.from_id_notation_to_query(args.board) if args.board is not None else None)
 
+  if args.board is not None and board_id_query_parameter is None:
+    query = f"[].boards[?name=='{args.board}'][]"
+  else:
+    query = f"[].boards[]"
+
+  boards = jmespath.search(query, db)
   filtered_boards = []
+
+  if board_id_query_parameter is not None:
+    boards = list(filter(lambda x: not not re.match(board_id_query_parameter, x['id']), boards))
 
   for board in boards:
     board = {k: board.get(k) for k in ('name', 'id')}
 
-    url = f'https://api.trello.com/1/board/{board["id"]}/cards'
-    response = requests.request('GET', url, headers=headers, params=query)
-    board['cards'] = [{k: card.get(k) for k in ('name', 'id')} for card in response.json()]
+    cards = api.boards.cards(board['id'], **vars(args))
+    board['cards'] = [{k: card.get(k) for k in ('name', 'id')} for card in cards]
 
     if not board['cards']:
       continue
