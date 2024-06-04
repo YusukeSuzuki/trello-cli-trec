b5823a7ef5901b27f95167f20eb858aba91d417f
Yusuke Suzuki <yusuke@geekfield.jp>
2024-06-04 15:42:53 +0900

    impl lists archive subcommand

--- trec/api/lists.py
+++ trec/api/lists.py
@@ -12,3 +12,14 @@ def cards(
   query = { 'key': api_key, 'token': api_token }
   response = requests.request('GET', url, headers=headers, params=query)
   return response.json()
+
+
+def archive_all(
+  list_id: str, *args: Any, api_key: str, api_token: str, **kwargs: Any,
+  ) -> Dict[str, any]:
+
+  url = f'https://api.trello.com/1/lists/{list_id}/archiveAllCards'
+  headers = { 'Accept': 'application/json' }
+  query = { 'key': api_key, 'token': api_token }
+  response = requests.request('POST', url, headers=headers, params=query)
+  return response.json()
--- trec/commands/lists/archive.py
+++ trec/commands/lists/archive.py
@@ -1,12 +1,37 @@
+import jmespath
+
+import trec.api as api
+import trec.data as data
+import trec.utils.trello as trello_util
+from trec.utils.api_keys import from_args as keys_from
+from trec.utils.jmespath import options as jmespath_options
+
+
 def name():
   return 'archive'
 
 def help():
-  return 'archive list'
+  return 'archive all cards in list'
 
 def implement(parser):
-  parser.add_argument('--board', '-b')
   parser.add_argument('list')
+  parser.add_argument('--dump', action='store_true', help='dump data into stdout')
 
 def process(args):
-  print('archive list of board(TBI)')
+  db = data.db.load_or_setup(**keys_from(args))
+
+  list_query_string = trello_util.query_for_list(args.list)
+  lists = jmespath.search(list_query_string, db, options=jmespath_options)
+
+  if not lists:
+    print('there is no target list')
+    return
+
+  responses = []
+
+  for target_list in lists:
+    response = api.lists.archive_all(target_list['id'], **keys_from(args))
+    responses.append(response)
+
+  if args.dump:
+    print(yaml.dump(responses, allow_unicode=True))
